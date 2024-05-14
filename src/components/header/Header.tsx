36b07e9a748ccf932ef437095cd262b30ed20834
AlexMakovetskiy <bo5634302@gmail.com>
2024-05-14 11:17:55 +0300

    init: add reset and global styles for app

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,7 @@
+import styles from "./Header.module.scss";
+
+function Header() {
+	return <header className={styles.headerWrap}>headerWrap</header>;
+}
+
+export default Header;
---
+++ src/pages/main/Main.tsx
@@ -0,0 +1,7 @@
+import styles from "./Main.module.scss";
+
+function Main() {
+	return <main className={styles.mainPageWrap}>mainPageWrap</main>;
+}
+
+export default Main;
